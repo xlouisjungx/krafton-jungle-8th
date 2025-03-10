b41693c1ec792426479e35706fd737ecb7eae75d
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-03-10 17:29:50 +0900

    design: main page design

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.jungle_sunday
+
 @app.route('/main')
 def main():
-  return render_template('main.html')
+    return render_template('main.html')
+
+@app.route('/')
+def post_get():
+    
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
