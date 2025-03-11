defac990347cdb5a47ca68064ba843af4a3d0d52
xlouisjungx <cutaandcrazy@gmail.com>
2025-03-11 19:25:53 +0900

    정리

--- README.md
+++
@@ -1,4 +0,0 @@
-# krafton-jungle-8th
-krafton jungle 8th mini project
-
-미니 프로젝트 메인 브랜치
--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/main')
-def main():
-  return render_template('main.html')
-
-if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
