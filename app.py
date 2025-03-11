f6e692d03a3847db6841201e1c0115330df1e4db
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-03-11 09:40:33 +0900

    feat: add user_info html

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
+from bson import ObjectId
+
 from flask import Flask, render_template, request, jsonify
+from flask.json.provider import JSONProvider
 app = Flask(__name__)
 
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.jungle_sunday
-#이미지, 게시자ID, 설명, 링크, 좋아요, 시간
 
-#db.post.insert_one({'image':'url', 'poster_id':'poster_id', 'explain': 'explain', 'link': 'link', 'likes': '0', 'post_time': 'post_time'})
+import json
 
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+    
+app.json = CustomJSONProvider(app)
+
+#MAIN HTML
 @app.route('/main')
 def main():
     return render_template('main.html',
@@ -17,10 +36,10 @@ def main():
                            post="글쓰기",
                            )
 
-@app.route('/main_post', methods=['GET', 'POST'])
+@app.route('/main/post', methods=['GET', 'POST'])
 def handle_post():
     if request.method == 'GET':
-      result = list(db.post.find({}, {'_id': 0}))
+      result = list(db.post.find({}))
       return jsonify({'result': 'success', 'posts': result})
     
     elif request.method == 'POST':
@@ -39,11 +58,28 @@ def handle_post():
         'link': link_receive,
         'post_time': post_time_receive,
         'like': 0,
+        'dislike': 0,
       }
 
       db.post.insert_one(post)
       return jsonify({'result': 'success'})
     
+@app.route('/main/post/like', methods=['POST'])
+def handle_like():
+  temp_id_receive = request.form['post_id_give']
+  id_receive = ObjectId(temp_id_receive)
+  db.post.update_one({'_id': id_receive}, {'$inc': {'like': 1}})
+  return jsonify({'result': 'success'})
+
+@app.route('/main/post/dislike', methods=['POST'])
+def handle_dislike():
+  temp_id_receive = request.form['post_id_give']
+  id_receive = ObjectId(temp_id_receive)
+  db.post.update_one({'_id': id_receive}, {'$inc': {'dislike': 1}})
+  return jsonify({'result': 'success'})
+
+
+#POST HTML
 @app.route('/post')
 def post():
    return render_template('post.html',
@@ -52,6 +88,13 @@ def post():
                           post="게시하기",
                           back="뒤로가기",
                           upload="사진 불러오기",
+                          )
+
+#USER_INFO HTML
+@app.route('/user_info')
+def user_info():
+   return render_template('user_info.html',
+                          title="사용자 정보",
                           )
 
 if __name__ == '__main__':  
