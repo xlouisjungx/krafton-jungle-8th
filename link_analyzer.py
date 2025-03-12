8afd836df56d563ca59744673ab5a9c60f87efe7
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-03-12 15:23:58 +0900

    fix: url redirect problem fix

--- link_analyzer.py
+++ link_analyzer.py
@@ -2,31 +2,51 @@ import requests
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
 #trouble shooting => beautiful soup로는 동적인 요소 로드 불가능 => selenium활용
 def analyze_link(url):
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행
+    # options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행 => 렌더링 문제로 띄우게 함.
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    driver.get(url)
-    time.sleep(1)
 
     place_data = {
         'title' : "",
         'place' : "",
         'phone_number' : ""
     }
+    # 링크가 다른 화면으로 리다이렉트 되는 경우 고려
+    driver.get(url)
+    time.sleep(2)
+    current_url = driver.current_url
 
     # 페이지 로딩 후 HTML 가져오기
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
     # 원하는 요소 선택
-    if "kakao" in url:
+    if "kakao" in current_url:
+        after_place = ''
+        if 'item' in current_url:
+            after_place = current_url.split("itemId=")[1]
+            new_url = 'https://place.map.kakao.com/' + after_place
+        else:
+            new_url=current_url
+        driver.get(new_url)
+        time.sleep(2)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
         place_data = kakao_analyzer(soup)
-    elif "google" in url:
-        place_data = google_analyzer(soup)
+    #네이버는 html안에 또 다른 html이 있음
+    elif "naver" in current_url:
+        after_place = ''
+        if 'restaurant' in current_url:
+            after_place = current_url.split("restaurant/")[1]
+        elif 'place' in current_url:
+            after_place = current_url.split("place/")[1]
+        base_url = 'https://pcmap.place.naver.com/restaurant/' + after_place
+        driver.get(base_url)
+        time.sleep(2)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        place_data = naver_analyzer(soup)
     else:
         place_data = other_analyzer(soup)
 
@@ -56,8 +76,13 @@ def kakao_analyzer(soup):
     place_data = place_data_changer(title, place, phone_number)
     return place_data
 
-def google_analyzer(soup):
-    i
+def naver_analyzer(soup):
+    title = soup.select_one('#_title > div > span.GHAhO').string.strip()
+    place = soup.select_one('#app-root > div > div > div > div:nth-child(5) > div > div:nth-child(2) > div > div > div.O8qbU.tQY7D > div > a > span.LDgIH').string.strip()
+    phone_number = soup.select_one('#app-root > div > div > div > div:nth-child(5) > div > div:nth-child(2) > div > div > div.O8qbU.nbXkr > div > span.xlx7Q').string.strip()
+
+    place_data = place_data_changer(title, place, phone_number)
+    return place_data
 
 def other_analyzer(soup):
     i
